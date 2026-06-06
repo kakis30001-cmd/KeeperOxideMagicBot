79b85a57ba277085fb409a0a22debe213767258b
kakis30001-cmd <kakis30001@gmail.com>
2026-06-07 02:43:51 +0300

    Update models.py

--- database/models.py
+++ database/models.py
@@ -1,9 +1,12 @@
-from sqlalchemy import BigInteger, String, ForeignKey, Boolean
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
-from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
 import os
+from sqlalchemy import BigInteger, String, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
 
-engine = create_async_engine(os.getenv('DB_URL'), echo=True)
+# Вставь сюда свой ID из @userinfobot
+ADMIN_IDS = [8493522297] 
+
+engine = create_async_engine(os.getenv('DB_URL', 'sqlite+aiosqlite:///db.sqlite3'), echo=False)
 async_session = async_sessionmaker(engine)
 
 class Base(AsyncAttrs, DeclarativeBase):
@@ -12,7 +15,7 @@ class Base(AsyncAttrs, DeclarativeBase):
 class User(Base):
     __tablename__ = 'users'
     id: Mapped[int] = mapped_column(primary_key=True)
-    tg_id = mapped_column(BigInteger)
+    tg_id = mapped_column(BigInteger, unique=True)
     balance: Mapped[int] = mapped_column(default=0)
 
 class Key(Base):
@@ -23,7 +26,6 @@ class Key(Base):
     product: Mapped[str] = mapped_column(String(50))
     key_code: Mapped[str] = mapped_column(String(100))
     is_sold: Mapped[bool] = mapped_column(default=False)
-    price: Mapped[int] = mapped_column(default=100) # Цена ключа
 
 async def async_main():
     async with engine.begin() as conn:
