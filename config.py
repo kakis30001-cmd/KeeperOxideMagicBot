bf0698b4bbc5884904bd0320c32f409358d00bc9
kakis30001-cmd <kakis30001@gmail.com>
2026-06-07 20:10:10 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,9 +6,5 @@ DB_URL = os.getenv("DB_URL")
 RAILWAY_URL = os.getenv("RAILWAY_URL")
 CHANNEL_ID = -1003709565134
 
-# Lava платежная система
-LAVA_SHOP_ID = os.getenv("LAVA_SHOP_ID")
-LAVA_SECRET_KEY = os.getenv("LAVA_SECRET_KEY")
-
 if DB_URL:
     DB_URL = DB_URL.replace("postgresql+asyncpg://", "postgresql://")
