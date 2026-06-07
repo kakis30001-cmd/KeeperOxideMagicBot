04ddc3875f1cb6a169be535783aa797b6bfbd143
kakis30001-cmd <kakis30001@gmail.com>
2026-06-07 21:07:06 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ DB_URL = os.getenv("DB_URL")
 RAILWAY_URL = os.getenv("RAILWAY_URL")
 CHANNEL_ID = -1003709565134
 
-PLATEGA_SHOP_ID = os.getenv("PLATEGA_SHOP_ID")
-PLATEGA_SECRET_KEY = os.getenv("PLATEGA_SECRET_KEY")
+MERCHANT_ID = os.getenv("MERCHANT_ID")
+API_SECRET = os.getenv("API_SECRET")
 
 if DB_URL:
     DB_URL = DB_URL.replace("postgresql+asyncpg://", "postgresql://")
