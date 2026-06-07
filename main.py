851175baba4146b7d51cef1b351286dfcfba7303
kakis30001-cmd <kakis30001@gmail.com>
2026-06-07 03:52:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import asyncio
-import logging
-import os
 from aiogram import Bot, Dispatcher
 from database.models import async_main
 from handlers.user import user_router
+from handlers.admin import admin_router
+import os
 
 async def main():
     await async_main()
     bot = Bot(token=os.getenv('BOT_TOKEN'))
     dp = Dispatcher()
-    dp.include_router(user_router)
+    dp.include_routers(admin_router, user_router)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
