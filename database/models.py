5d3b5621cbb49cfe89aef6821c095ddf7c583b2e
kakis30001-cmd <kakis30001@gmail.com>
2026-06-07 03:51:18 +0300

    Update models.py

--- database/models.py
+++ database/models.py
@@ -1,17 +1,12 @@
 import os
-from sqlalchemy import BigInteger, String, Boolean, ForeignKey, Integer, MetaData
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+from sqlalchemy import BigInteger, String, Boolean, ForeignKey, Integer
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
+from sqlalchemy.ext.asyncio import AsyncAttrs, create_async_engine, AsyncSession
 
-# Принудительно задаем значение по умолчанию, если переменная в Railway отсутствует
-DATABASE_URL = os.getenv('DATABASE_URL') or 'sqlite+aiosqlite:///db.sqlite3'
+engine = create_async_engine(os.getenv('DATABASE_URL'))
+async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
-# Создаем движок
-engine = create_async_engine(DATABASE_URL, echo=False)
-async_session = async_sessionmaker(engine, expire_on_commit=False)
-
-class Base(AsyncAttrs, DeclarativeBase):
-    metadata = MetaData()
+class Base(AsyncAttrs, DeclarativeBase): pass
 
 class User(Base):
     __tablename__ = 'users'
@@ -20,20 +15,18 @@ class User(Base):
 
 class Product(Base):
     __tablename__ = 'products'
-    id: Mapped[str] = mapped_column(String(50), primary_key=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(100))
     price: Mapped[int] = mapped_column(Integer)
 
 class Key(Base):
     __tablename__ = 'keys'
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    product_id: Mapped[str] = mapped_column(String(50), ForeignKey('products.id'))
+    product_id: Mapped[int] = mapped_column(ForeignKey('products.id'))
     key_code: Mapped[str] = mapped_column(String(255))
     is_sold: Mapped[bool] = mapped_column(Boolean, default=False)
 
 async def async_main():
     async with engine.begin() as conn:
-        # Создает таблицы только если их нет. 
-        # Ошибки из-за "None" теперь быть не может.
         await conn.run_sync(Base.metadata.create_all)
         
