9dcd5b40feb1c1f03361d1bf1fad13d5e7253d01
kakis30001-cmd <kakis30001@gmail.com>
2026-06-07 03:11:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import asyncio
 import os
+import logging
 from aiogram import Bot, Dispatcher
 from database.models import async_main
 from handlers.user import user_router
 from handlers.admin import admin_router
 
 async def main():
+    logging.basicConfig(level=logging.INFO)
     await async_main()
-    # Получаем токен из переменных окружения Railway
+    
     bot = Bot(token=os.getenv('BOT_TOKEN'))
     dp = Dispatcher()
     
-    # Подключаем оба роутера
     dp.include_router(user_router)
     dp.include_router(admin_router)
     
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
     asyncio.run(main())
-    
