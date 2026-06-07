d389c98d68b3d2a5ba16eef017d59e2483064793
kakis30001-cmd <kakis30001@gmail.com>
2026-06-07 10:44:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,9 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
-
 DB_URL = os.getenv("DB_URL")
-DB_URL = DB_URL.replace("postgresql+asyncpg://", "postgresql://")
+RAILWAY_URL = os.getenv("RAILWAY_URL")
+
+# Для PostgreSQL
+if DB_URL:
+    DB_URL = DB_URL.replace("postgresql+asyncpg://", "postgresql://")
