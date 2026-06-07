1d896a423021365f24778aeeb64fea8fdf804839
kakis30001-cmd <kakis30001@gmail.com>
2026-06-07 19:54:08 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
 DB_URL = os.getenv("DB_URL")
 RAILWAY_URL = os.getenv("RAILWAY_URL")
-CHANNEL_ID = -1003709565134
 
 if DB_URL:
     DB_URL = DB_URL.replace("postgresql+asyncpg://", "postgresql://")
