38451ee3d2a86e02749316628eeb3d641f0f4d51
kakis30001-cmd <kakis30001@gmail.com>
2026-06-08 11:58:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_IDS = [int(id.strip()) for id in os.getenv("ADMIN_IDS", "").split(",") if id.strip()]
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
 DB_URL = os.getenv("DB_URL")
 RAILWAY_URL = os.getenv("RAILWAY_URL")
 CHANNEL_ID = -1003709565134
