9fb6d5972bba904f88af67cbef900f79572471da
kakis30001-cmd <kakis30001@gmail.com>
2026-06-08 12:13:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
+ADMIN_IDS = [int(id.strip()) for id in os.getenv("ADMIN_IDS", "").split(",") if id.strip()]
 DB_URL = os.getenv("DB_URL")
 RAILWAY_URL = os.getenv("RAILWAY_URL")
 CHANNEL_ID = -1003709565134
+
+MERCHANT_ID = os.getenv("MERCHANT_ID")
+API_SECRET = os.getenv("API_SECRET")
 
 if DB_URL:
     DB_URL = DB_URL.replace("postgresql+asyncpg://", "postgresql://")
