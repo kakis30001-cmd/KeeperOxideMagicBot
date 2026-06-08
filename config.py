a66f16085206c365c98a7385fec14abd2deff4d2
kakis30001-cmd <kakis30001@gmail.com>
2026-06-08 11:16:00 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,5 +9,10 @@ CHANNEL_ID = -1003709565134
 MERCHANT_ID = os.getenv("MERCHANT_ID")
 API_SECRET = os.getenv("API_SECRET")
 
+CRYPTO_PAYMENT_MODE = os.getenv("CRYPTO_PAYMENT_MODE", "auto")  
+CRYPTO_MANUAL_TEXT = os.getenv("CRYPTO_MANUAL_TEXT", "Для оплаты криптовалютой переведите средства на кошелек USDT TRC20: TXXXX... и отправьте скриншот и хэш перевода администратору.")
+CRYPTO_CURRENCY = os.getenv("CRYPTO_CURRENCY", "USDT")
+CRYPTO_AMOUNT = int(os.getenv("CRYPTO_AMOUNT", "10"))
+
 if DB_URL:
     DB_URL = DB_URL.replace("postgresql+asyncpg://", "postgresql://")
